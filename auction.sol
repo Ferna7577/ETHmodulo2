8435b16756bc37cac060e23ce5fca443baeed90d
Ferna7577 <117449546+Ferna7577@users.noreply.github.com>
2025-06-09 21:16:39 -0300

    Update auction2.sol

--- auction.sol
+++ auction.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-
+// Versión 2
 pragma solidity >=0.8.2 <0.9.0;
 
 contract auction {
@@ -18,8 +18,6 @@ contract auction {
         owner = _auction;
         endAuction = block.timestamp + duration;
     }
-
-    receive() external payable {}
 
     /*Evento que registra la dirección y monto de la oferta mayor*/
     event altaOferta(address indexed userAddress, uint256 userId);
@@ -43,8 +41,7 @@ contract auction {
         require(!auctionClosed, "subasta cerrada");
         require((block.timestamp < endAuction), "Tiempo agotado");
         require(msg.value >= (higherOffer + higherOffer * 5 / 100), ("la oferta debe ser mayor"));
-        (bool result, ) = msg.sender.call{value:msg.value}("");
-        require(result);
+        
         if (offers[msg.sender] != 0){
             (bool result2, ) = msg.sender.call{value:offers[msg.sender]}("");
             require(result2);
@@ -62,34 +59,17 @@ contract auction {
     /* Función returnBid, permite devolver las ofertas que no ganaron:*/
     function returnBid() onlyOwner external payable{
         require((auctionClosed = false), "aun activa" );
+        /*Pone la oferta más alta del mapping 'offers' en 0 para no devolver al ganador pero si a los perdedores*/
+        offers[addressHigherOffer] = 0;
+        /* Recorre el array 'biddersHigherOffer' donde extrae las direcciones de los oferentes y las usa,
+        para sacar el valor de la oferta de esa dirección del mapping 'offers'.*/
         for(uint i = 0 ; i < biddersHigherOffer.length ; ++i) {
-            if (biddersHigherOffer[i] != addressHigherOffer) {
-                bidRestComision = (offers[biddersHigherOffer[i]]) - (offers[biddersHigherOffer[i]]) * 2 /100;
-                (bool result3, ) = biddersHigherOffer[i].call{value:bidRestComision}("");
-                require(result3);
+            /* Saca el valor de la dirección a devolver y le resta el 2%*/
+            bidRestComision = (offers[biddersHigherOffer[i]]) - ((offers[biddersHigherOffer[i]]) * 2 /100);
+            /* Devuelve el valor anterior a la dirección que esta recorriendo*/
+            (bool result3, ) = biddersHigherOffer[i].call{value:bidRestComision}("");
+            require(result3);
             }
 
-    
-
-        }
     }
 }
-/* 
-******Función bid, permite hacer ofertas y valida:
-- que la subasta esta activa.
-- que no se haya agotado el tiempo.
-- que la oferta sea mayor que la ultima por un 5%.
-- que el call a receive se complete.
-- el if comprueba que el oferente ya había ofertado y logra la mayor oferta se le devuelve la anterior.
-esto asegura que cada oferente tiene una sola oferta y agiliza la devolución al finalizar la subasta.
-- se todo es válido guarda la address y el mayor valor de esa address en un mapping 'offers'.
-- si todo es válido registra en una variable la mayor oferta 'higherOffer'.
-- guarda las address de los oferentes en un array 'bidders' para su posterior devolución cuando finalice.
-- Emite un evento cuando hay una oferta mayor con la direccion y el monto.
-- Comprueba si la oferta esta en los últimos 10 min (600seg), si es si agrega otros 10 min. 
- 
-******Función returnBid, permite devolver las ofertas que no ganaron:
-- valida que solo el dueño de la subasta pueda llamar la fucnión con el modificador onlyOwner.
-- valida que la subasta ya este acabada.
-- devuelve las ofertas perdedoras a sus dueños menos el 2% de comisión.
-*/
